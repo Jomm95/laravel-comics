88eb05bdf637580f6d1a42b70b6d849ed710640e
Thomas <venturini.thomas95@gmail.com>
2022-03-29 17:17:00 +0200

    .

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('pageTitle')</title>
+    {{-- collegamento tramite asset alla cartella public e al css compilato  --}}
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
 
